48418d5662d2abb413fa825fbb2d04d41b7a3cac
RobertJN64 <robertjnies@gmail.com>
2023-11-06 11:31:16 -0500

    recalibrated, render working with accurate tag size

--- calibrate_cam.py
+++ calibrate_cam.py
@@ -41,10 +41,9 @@ for fname in images:
         # Draw and display the corners
         img = cv2.drawChessboardCorners(img, CHECKERBOARD, corners2, ret)
 
-    cv2.imshow('img', img)
+    cv2.imshow(fname, img)
     cv2.waitKey(0)
-
-cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
 
 h, w = img.shape[:2]
 

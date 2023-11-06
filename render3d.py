859a9b68afc906b27d1e44df10b73e8af33f474a
RobertJN64 <robertjnies@gmail.com>
2023-11-06 11:14:10 -0500

    fixes axes

--- render3d.py
+++ render3d.py
@@ -9,21 +9,22 @@ def create_env():
 def update(tags, ax):
     ax.clear()
     def render_tag(x, y, z):
-        ax.plot([x, x, x, x, x],
-                [y-0.5, y-0.5, y+0.5, y+0.5, y-0.5],
-                [z-0.5, z+0.5, z+0.5, z-0.5, z-0.5])
+        ax.plot([x-0.5, x+0.5, x+0.5, x-0.5, x-0.5],
+                [z, z, z, z, z], #camera z = y
+                [y-0.5, y-0.5, y+0.5, y+0.5, y-0.5]) #camera y = z
 
     for tag in tags:
+        tag[1] = -tag[1]
         render_tag(*tag)
 
     tags.append([0,0,0]) #camera pos
 
     ax.set_xlim(min([tag[0] for tag in tags]) - 10, max([tag[0] for tag in tags]) + 10)
-    ax.set_ylim(min([tag[1] for tag in tags]) - 10, max([tag[1] for tag in tags]) + 10)
-    ax.set_zlim(min([tag[2] for tag in tags]) - 10, max([tag[2] for tag in tags]) + 10)
+    ax.set_ylim(min([tag[2] for tag in tags]) - 10, max([tag[2] for tag in tags]) + 10)
+    ax.set_zlim(min([tag[1] for tag in tags]) - 10, max([tag[1] for tag in tags]) + 10)
 
     #ax.scatter3D([0],[0],[0], s=10)
-    ax.quiver(0, 0, 0, 5, 0, 0)
+    ax.quiver(0, 0, 0, 0, 5, 0)
     plt.draw()
     plt.pause(0.01)
 

8951083b7986981cd849b0b8f41d08c6c67f6c0c
RobertJN64 <robertjnies@gmail.com>
2023-11-25 02:06:29 -0500

    refactor naming

--- tools/object_map.py
+++ tools/object_map.py
@@ -1,33 +1,35 @@
 from typing import Self
 
 class ObjectMap:
-    def __init__(self, obj):
+    def __init__(self, name, obj):
+        self.name = name
+        self.obj = obj
         self.func_dict: dict[str, tuple] = {} #tuple of function(), args
-        self.var_dict: dict[str, tuple] = {} #tuple of baseobject, varname
+        self.var_list = [] #list of varnames
         self.subobj_dict: dict[str, Self] = {} #ObjectMap of subobject
 
-        for name in [x for x in dir(obj) if x[0] != '_']:
-            inst = getattr(obj, name)
+        for attr in [x for x in dir(obj) if x[0] != '_']:
+            inst = getattr(obj, attr)
             if hasattr(inst, '__call__'):
-                self.func_dict[name] = (inst, inst.__code__.co_varnames[1:]) #ignore self param
+                self.func_dict[attr] = (inst, inst.__code__.co_varnames[1:]) #ignore self param
             elif isinstance(inst, (int, float, str, bool)):
-                self.var_dict[name] = (obj, name)
+                self.var_list.append(attr)
             else:
                 # noinspection PyTypeChecker
-                self.subobj_dict[name] = self.__class__(inst) #this allows superclassing
+                self.subobj_dict[attr] = self.__class__(self.name + '.' + attr, inst) #this allows superclassing
 
-    def call_function(self, func_trace, kwargs):
-        obj_name = func_trace[0]
-        func_trace = func_trace[1:]
-        if len(func_trace) == 0:
-            self.func_dict[obj_name][0](**kwargs)
-        else:
+    def call_function(self, func_trace: str, kwargs):
+        func_name = func_trace.removeprefix(self.name + '.')
+        if '.' in func_name:
+            obj_name = func_name.split('.')[0]
             self.subobj_dict[obj_name].call_function(func_trace, kwargs)
+        else:
+            self.func_dict[func_name][0](**kwargs)
 
     def __repr__(self):
-        return (f"Object with:"
+        return (f"{self.name} with:"
                 f"\nfunctions:\n{self.func_dict}" +
-                f"\nvars:\n{self.var_dict}"
+                f"\nvars:\n{self.var_list}"
                 f"\nsubobjects: {list(self.subobj_dict.keys())}")
 
 
--- tools/web_object_map.py
+++ tools/web_object_map.py
@@ -4,52 +4,62 @@ import flask
 ## LIMITATION - ASSUMES ALL PARAMS ARE INTS
 
 class WebObjectMap(ObjectMap):
-    def generate_html(self, name, font_size=2.5):
-        subcontent = ""
-        for subname, om in self.subobj_dict.items():
-            subcontent += om.generate_html(name + '.' + subname, font_size - 0.25) + '\n'
-
+    def generate_html(self, font_size=2.5):
         buttons = ""
         for funcname, (f, params) in self.func_dict.items():
-            onclick = f'quick_action("{name + "." + funcname}")'
+            stack = self.name + "." + funcname
+            onclick = f'quick_action("{stack}")'
             buttons += f"<button class='nice_button' onclick={onclick}>{funcname}</button>"
             for paramname in params:
-                className = 'py_' + name + '.' + funcname
-                elemID = 'py_' + name + '.' + funcname + '|' + paramname
+                className = 'py_' + stack
+                elemID = className + '|' + paramname
                 buttons += f"<span class='input_label'>{paramname}</span>"
                 buttons += f"<input id='{elemID}' class='{className}' type='number' value='0'>"
-            buttons += "<br><br>"
+            buttons += "<br>"
+
+        var_table = ""
+        if len(self.var_list) > 0:
+            var_table += "<br><table><thead><tr><th>Variable</th><th>Value</th></thead><body>"
+            for varname in self.var_list:
+                elemID = 'py_' + self.name + "." + varname
+                var_table += f"<tr><td>{varname}</td><td id='{elemID}'>Waiting for update...</td>"
+            var_table += "</body></table>"
+
+        subcontent = ""
+        for subname, om in self.subobj_dict.items():
+            subcontent += om.generate_html(font_size - 0.5) + '\n'
 
         return (f"""
         <details open>
-            <summary style="font-size: {font_size}em">{name}</summary>
+            <summary style="font-size: {font_size}em">{self.name}</summary>
             {buttons}
+            {var_table}
             {subcontent}
         </details>
         """)
 
     def get_status(self):
         update = {}
-        for name, (baseobject, attrname) in self.var_dict.items():
-            update[name] = getattr(baseobject, attrname)
+        for var_name in self.var_list:
+            update[var_name] = getattr(self.obj, var_name)
         for name, obj in self.subobj_dict.items():
             for sub_name, ref in obj.get_status().items():
                 update[name + '.' + sub_name] = ref
         return update
 
 def create_WebObjectMap_server(flask_app, name, obj):
-    wom = WebObjectMap(obj)
+    wom = WebObjectMap(name, obj)
 
-    py_control_content = wom.generate_html(name)
+    py_control_content = wom.generate_html()
 
     @flask_app.route('/py_control')
     def py_control():
         return flask.render_template('py_control.html', content=py_control_content)
 
     @flask_app.route('/quick_action/<action>')
-    def quick_action(action: str):
+    def quick_action(action):
         kwargs = {argname: int(arg) for argname, arg in flask.request.args.items()}
-        wom.call_function(action.removeprefix(name + '.').split('.'), kwargs)
+        wom.call_function(action, kwargs)
         return "OK"
 
 
